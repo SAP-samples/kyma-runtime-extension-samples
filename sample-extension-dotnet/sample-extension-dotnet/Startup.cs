df9aacabd5f75d8f48e11c63ce2eed3036a2e0f8
Gaurav Abbi <abbi.gaurav@gmail.com>
2020-07-30 11:17:10 +0200

    making it lightweight

--- sample-extension-dotnet/sample-extension-dotnet/Startup.cs
+++ sample-extension-dotnet/sample-extension-dotnet/Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace sample_extension_dotnet
 {
@@ -23,7 +19,6 @@ namespace sample_extension_dotnet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,17 +34,9 @@ namespace sample_extension_dotnet
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.Run(async (context) =>
             {
-                endpoints.MapRazorPages();
+                await context.Response.WriteAsync("Hello World");
             });
         }
     }
