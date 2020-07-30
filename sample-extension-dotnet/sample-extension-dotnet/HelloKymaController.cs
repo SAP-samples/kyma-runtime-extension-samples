fbdc739da2cd2eb486519e38970a5718cbb5b6aa
Gaurav Abbi <abbi.gaurav@gmail.com>
2020-07-30 11:49:43 +0200

    minor clean up

---
+++ sample-extension-dotnet/sample-extension-dotnet/HelloKymaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sample_extension_dotnet
+{
+    public class HelloKymaController : Controller
+    {
+        public HelloKymaController()
+        {
+        }
+
+        //GET /helloworld
+        public String Index()
+        {
+            return "Hello from dotnet app running on Kyma Runtime";
+        }
+    }
+}
