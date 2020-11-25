bae21dc36d46a810f862297cf5ef49482d5703f3
Gaurav Abbi <abbi.gaurav@gmail.com>
2020-11-25 13:52:57 +0100

    working micronaut sample

--- sample-micronaut/src/main/java/com/sap/kyma/sample/orders/domain/command/CreateOrder.java
+++ sample-micronaut/src/main/java/com/sap/kyma/sample/orders/domain/command/CreateOrder.java
@@ -1,11 +1,17 @@
-package sample.micronaut.domain.command;
+package com.sap.kyma.sample.orders.domain.command;
 
 import javax.validation.constraints.NotBlank;
 
 public class CreateOrder {
-
     @NotBlank
     private String description;
+
+    public CreateOrder() {
+    }
+
+    public CreateOrder(String description) {
+        this.description = description;
+    }
 
     public String getDescription() {
         return description;
--- sample-micronaut/src/main/java/com/sap/kyma/sample/orders/domain/command/UpdateOrder.java
+++ sample-micronaut/src/main/java/com/sap/kyma/sample/orders/domain/command/UpdateOrder.java
@@ -1,8 +1,16 @@
-package sample.micronaut.domain.command;
+package com.sap.kyma.sample.orders.domain.command;
 
 import javax.validation.constraints.NotBlank;
 
 public class UpdateOrder {
+    public UpdateOrder() {
+
+    }
+
+    public UpdateOrder(String description) {
+        this.description = description;
+    }
+
     @NotBlank
     private String description;
 
