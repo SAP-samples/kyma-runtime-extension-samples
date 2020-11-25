542fbff009586b67d36bab34054f6e0c63c44259
Gaurav Abbi <abbi.gaurav@gmail.com>
2020-11-25 17:56:56 +0100

    renamed file

--- sample-micronaut/README.md
+++
@@ -1,4 +0,0 @@
-## Feature http-client documentation
-
-- [Micronaut Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
-
--- sample-micronaut/src/main/java/com/sap/kyma/sample/orders/config/ApplicationConfig.java
+++
@@ -1,7 +0,0 @@
-package com.sap.kyma.sample.orders.config;
-
-import io.micronaut.context.annotation.ConfigurationProperties;
-
-@ConfigurationProperties("application")
-public class ApplicationConfig {
-}
