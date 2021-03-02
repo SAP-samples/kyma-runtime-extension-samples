bcd8d90998eb2d0361ddc66e875520669d833281
GAURAV ABBI <gaurav.abbi@example.com>
2021-03-02 14:56:47 +0100

    wip

--- sample-websockets/frontend/src/assets/env.js
+++ sample-websockets/frontend/src/assets/env.js
@@ -2,8 +2,8 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["issuer"] = "https://issuer.url";
-  window["env"]["clientId"] = "provide-clientId";
-  window["env"]["backendApiUrl"] = "https://backend.url";
-  window["env"]["backendWsUrl"] = "wss://backend.url";
+  window["env"]["issuer"] = "https://kyma-quest.eu.auth0.com/";
+  window["env"]["clientId"] = "5qXkgnXJT36rDyibq4FzBIT9TfGx2oCD";
+  window["env"]["backendApiUrl"] = "https://tornado-allow.ed72781.kyma-stage.shoot.live.k8s-hana.ondemand.com/ws";
+  window["env"]["backendWsUrl"] = "wss://tornado-allow.ed72781.kyma-stage.shoot.live.k8s-hana.ondemand.com/ws";
 })(this);
