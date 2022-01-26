1c55abb76a7c4505225c65fbad254fa15b09e185
Gaurav Abbi <gaurav.abbi@sap.com>
2022-01-26 14:18:27 +0100

    Minor fix to handle errors when adding questions

--- chatbot-conversational_AI/update-bot/app/update-bot.js
+++ chatbot-conversational_AI/update-bot/app/update-bot.js
@@ -1,5 +1,5 @@
-const             sql = require('mssql');
-const             got = require('got');
+const sql = require('mssql');
+const got = require('got');
 const TurndownService = require('turndown');
 
 async function main() {
@@ -15,18 +15,18 @@ async function main() {
 
     if (stackQuestion.is_answered) {
       var answers = await get_stackAnswers(stackQuestion.question_id),
-          questionText = stackQuestion.title;
-          questionLink = stackQuestion.link;
+        questionText = stackQuestion.title;
+      questionLink = stackQuestion.link;
       var bestAnswer = answers.items[0],
-          answerText = bestAnswer.body;
-    
+        answerText = bestAnswer.body;
+
       var stack_q_id = stackQuestion.question_id,
-          stack_q_ts = stackQuestion.last_activity_date
-          stack_a_id = bestAnswer.answer_id,
-          stack_a_ts = bestAnswer.last_edit_date ? bestAnswer.last_edit_date : bestAnswer.creation_date
-          cai_q_id = null,
-          cai_a_id = null;
-      
+        stack_q_ts = stackQuestion.last_activity_date
+      stack_a_id = bestAnswer.answer_id,
+        stack_a_ts = bestAnswer.last_edit_date ? bestAnswer.last_edit_date : bestAnswer.creation_date
+      cai_q_id = null,
+        cai_a_id = null;
+
       // Check if the question is already in the database
       var q_in_db_flag = 0;
       var id_in_db = -1;
@@ -45,7 +45,7 @@ async function main() {
           await delete_caiEntry(dbData[id_in_db].cai_a_id, caiCredentials.access_token);
           await db_request.query(`delete from Questions where id_num = '${dbData[id_in_db].id_num}'; `);
           q_update_flag = 1;
-        } catch(err) {
+        } catch (err) {
           console.log("An Error has occurred during deleting a Question/Answer pair in SAP CAI and the internal DB");
           throw err;
         }
@@ -54,11 +54,15 @@ async function main() {
       // Question is not in database or must be updated (was deleted and must be added again)
       if (q_in_db_flag == 0 || q_update_flag == 1) {
         try {
-          var caiAnswerResult = await add_caiAnswer(answerText, questionLink, caiCredentials.access_token);
-          cai_a_id = caiAnswerResult.results.id;
-          var caiQuestionResult = await add_caiQuestion(questionText, cai_a_id, caiCredentials.access_token);
-          cai_q_id = caiQuestionResult.results.id;
-          await db_request.query(`insert into Questions (stack_q_id, stack_q_ts, stack_a_id, stack_a_ts, cai_q_id, cai_a_id) values ('${stack_q_id}', '${stack_q_ts}', '${stack_a_id}', '${stack_a_ts}', '${cai_q_id}', '${cai_a_id}'); select * from Questions where stack_q_id = '${stack_q_id}'`);
+          var arrTuple = await add_caiAnswer(answerText, questionLink, caiCredentials.access_token);
+          var caiAnswerResult = arrTuple[0];
+          var errValue = arrTuple[1];
+          if (errValue === null) {
+            cai_a_id = caiAnswerResult.results.id;
+            var caiQuestionResult = await add_caiQuestion(questionText, cai_a_id, caiCredentials.access_token);
+            cai_q_id = caiQuestionResult.results.id;
+            await db_request.query(`insert into Questions (stack_q_id, stack_q_ts, stack_a_id, stack_a_ts, cai_q_id, cai_a_id) values ('${stack_q_id}', '${stack_q_ts}', '${stack_a_id}', '${stack_a_ts}', '${cai_q_id}', '${cai_a_id}'); select * from Questions where stack_q_id = '${stack_q_id}'`);
+          }
         } catch (err) {
           console.log("An Error has occurred during adding a new Question/Answer pair to SAP CAI and the internal DB");
           throw err;
@@ -71,7 +75,7 @@ async function main() {
           try { // Delete it from the database and CAI
             await delete_caiEntry(dbData[i].cai_a_id, caiCredentials.access_token);
             await db_request.query(`delete from Questions where id_num = '${dbData[i].id_num}'`);
-          } catch(err) {
+          } catch (err) {
             console.log("An Error has occurred during deleting a Question/Answer pair in SAP CAI and the internal DB (it was deleted because it was deleted in Stack Overflow)");
             throw err;
           }
@@ -113,7 +117,7 @@ async function get_stackQuestions() {
     }
 
     return allQuestions;
-  } catch(err) {
+  } catch (err) {
     console.log("An Error has occurred during requesting the stack questions labeled with " + process.env.STACK_TAG + ". Maybe it is a problem with concatenating multiple pages of questions because max pagesize exceeded.");
     throw err;
   }
@@ -124,7 +128,7 @@ async function get_stackAnswers(question_id) {
   try {
     const result = await got(stack_url_answers, stack_config);
     return result.body;
-  } catch(err) {
+  } catch (err) {
     console.log("An Error has occurred during requesting the stack answer to question " + question_id);
     throw err;
   }
@@ -148,7 +152,7 @@ async function get_dbData(db_request) {
   try {
     const result = await db_request.query('select * from Questions');
     return result.recordsets[0];
-  } catch(err) {
+  } catch (err) {
     console.log("An Error has occurred during requesting the database content");
     throw err;
   }
@@ -174,7 +178,7 @@ async function get_caiCredentials() {
   try {
     const result = await got(cai_credentials_url, cai_config);
     return JSON.parse(result.body);
-  } catch(err) {
+  } catch (err) {
     console.log("An Error has occurred during requesting the cai credentials");
     throw err;
   }
@@ -193,7 +197,7 @@ const cai_request_config = {
 
 async function add_caiAnswer(answerText, questionLink, access_token) {
   try {
-    cai_request_config.headers.Authorization =  'Bearer ' + access_token;
+    cai_request_config.headers.Authorization = 'Bearer ' + access_token;
 
     // convert html to markdown
     var turndownService = new TurndownService();
@@ -205,32 +209,37 @@ async function add_caiAnswer(answerText, questionLink, access_token) {
     slackFormat = slackFormat.replace(/\*\*/g, '\*');
 
     // add the Link to Stack Overflow to the Answer
-    if (slackFormat.length > 2050) { // answer is too long
+    if (slackFormat.length > 1800) { // answer is too long
       finalFormat = "\n" + slackFormat.substring(0, slackFormat.split('. ', 4).join('. ').length + 1) + " ... " + " <" + questionLink + "/|[See more]>";
     } else {
       var finalFormat = "\n" + slackFormat + "\n\n" + "For more help, please click <" + questionLink + "/|here> to go directly to this question on Stack Overflow.";
     }
 
-    cai_request_config.body = '{"value": ' + JSON.stringify(finalFormat)  + '}';
+    var finalAnswerString = JSON.stringify(finalFormat);
+    if (finalAnswerString.length > 2500) {
+      finalAnswerString = finalAnswerString.substring(0, 2500);
+    }
+    cai_request_config.body = '{"value": ' + finalAnswerString + '}';
     const result = await got.post(cai_request_url, cai_request_config);
-    return JSON.parse(result.body); 
-  } catch(err) {
-    console.log("An Error has occurred during adding an answer to SAP CAI");
-    throw err;
+    return [JSON.parse(result.body), null];
+  } catch (err) {
+    console.log("An Error has occurred during adding an answer to SAP CAI: " + err);
+    //throw err;
+    return [null, err];
   }
 }
 
 async function add_caiQuestion(questionText, answerID, access_token) {
   try {
-    cai_request_config.headers.Authorization =  'Bearer ' + access_token;
+    cai_request_config.headers.Authorization = 'Bearer ' + access_token;
     questionText = questionText.replace(/\&quot\;/g, "\"");
     questionText = questionText.replace(/.com\/\?/g, ".com? ");
 
-    cai_request_config.body = '{"value": ' + JSON.stringify(questionText)  + ', "display": "true"}';
+    cai_request_config.body = '{"value": ' + JSON.stringify(questionText) + ', "display": "true"}';
     cai_request_url_with_question = cai_request_url + '/' + answerID + '/questions';
     const result = await got.post(cai_request_url_with_question, cai_request_config);
     return JSON.parse(result.body);
-  } catch(err) {
+  } catch (err) {
     console.log("An Error has occurred during adding a question to SAP CAI");
     throw err;
   }
@@ -238,11 +247,11 @@ async function add_caiQuestion(questionText, answerID, access_token) {
 
 async function delete_caiEntry(answerID, access_token) {
   try {
-    cai_request_config.headers.Authorization =  'Bearer ' + access_token;
+    cai_request_config.headers.Authorization = 'Bearer ' + access_token;
     cai_request_url_with_id = cai_request_url + '/' + answerID;
     const result = await got.delete(cai_request_url_with_id, cai_request_config);
     return JSON.parse(result.body);
-  } catch(err) {
+  } catch (err) {
     console.log("An Error has occurred during deleting a question from SAP CAI");
     throw err;
   }
