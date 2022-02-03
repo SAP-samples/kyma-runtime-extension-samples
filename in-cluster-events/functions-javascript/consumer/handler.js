b5ae782047e2d2ba41231f6a129cbf9a10bb529e
Gaurav Abbi <gaurav.abbi@sap.com>
2022-02-03 13:29:36 +0100

    refine consumer function

--- in-cluster-events/functions-javascript/consumer/handler.js
+++ in-cluster-events/functions-javascript/consumer/handler.js
@@ -1,6 +1,6 @@
 module.exports = { 
     main: function (event, context) {
-      console.log(`event data: ${event.data}`);
+      console.log(`event data: ${JSON.stringify(event.data)}`);
       console.log(`headers: ${JSON.stringify(event.extensions.request.headers)}`);
     }
   }
