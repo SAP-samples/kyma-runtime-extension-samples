6361a3842098a3c89e1d28d67e891d36ebab40d5
Gaurav Abbi <gaurav.abbi@sap.com>
2022-02-03 11:06:48 +0100

    add producer

---
+++ in-cluster-events/functions-javascript/producer/handler.js
@@ -0,0 +1,26 @@
+const axios = require("axios").default;
+const { HTTP, CloudEvent } = require("cloudevents");
+
+module.exports = {
+    main: async function (event, context) {
+        const ce = new CloudEvent({
+            type: "sap.kyma.custom.internal.product.viewed.v1",
+            source: "/default/my.kyma/gaurav-10",
+            data: { 'produtId': '123' },
+            datacontenttype: "application/json"
+        },
+        );
+
+        const message = HTTP.structured(ce);
+        console.log(`ce: ${JSON.stringify(message)}`);
+        console.log(`printing content type ${JSON.stringify(message.headers['content-type'])}`);
+
+        var publishResponse = await axios({
+            method: 'post',
+            url: process.env.PUBLISHER_URL,
+            data: message.body,
+            headers: message.headers
+        })
+        console.log(publishResponse);
+    }
+}
