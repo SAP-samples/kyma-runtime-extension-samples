3f6cac4f23151ba37e40152708c770aea84cbecf
Jamie Cawley <jamie.cawley@sap.com>
2022-06-15 08:24:15 -0400

    version updates

--- user-propagation-via-xsuaa/angular-example-app/src/app/c4c-task/c4c-task.component.spec.ts
+++ user-propagation-via-xsuaa/angular-example-app/src/app/c4c-task/c4c-task.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { C4cTaskComponent } from './c4c-task.component';
 
@@ -6,7 +6,7 @@ describe('C4cTaskComponent', () => {
   let component: C4cTaskComponent;
   let fixture: ComponentFixture<C4cTaskComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ C4cTaskComponent ]
     })
--- user-propagation-via-xsuaa/angular-example-app/src/app/headers/headers.component.spec.ts
+++ user-propagation-via-xsuaa/angular-example-app/src/app/headers/headers.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeadersComponent } from './headers.component';
 
@@ -6,7 +6,7 @@ describe('HeadersComponent', () => {
   let component: HeadersComponent;
   let fixture: ComponentFixture<HeadersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeadersComponent ]
     })
