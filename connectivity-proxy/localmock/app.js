fd8c92d25e189c0ff2342ce5c58dfaf6dc7ecd72
Jamie Cawley <jamie.cawley@sap.com>
2022-08-03 15:22:31 -0400

    Update app.js

--- connectivity-proxy/localmock/app.js
+++ connectivity-proxy/localmock/app.js
@@ -10,41 +10,34 @@ app.listen(3000, () => {
 });
 
 app.post("/orders", (req, res, next) => {
-  res.json({
-    OrderNo: (req.body.orderNo || 1).toString(),
-    createdBy: "Internal Inc.",
-    buyer: "The Buyer Co.",
-    currency: { code: "EUR" },
-    Items: [
-      {
-        product_ID: getRandomInt(1000, 1000000).toString(),
-        quantity: getRandomInt(1, 10),
-        title: randomWords(),
-        price: getRandomInt(10, 20),
-      },
-    ],
-  });
+  const order = (req.body.orderNo || 1).toString();
+  res.json(createOrder(order));
 });
 
 app.get("/orders", (req, res, next) => {
-  res.json({
-    OrderNo: getRandomInt(10, 20).toString(),
-    createdBy: "Internal Inc.",
-    buyer: "The Buyer Co.",
-    currency: { code: "EUR" },
-    Items: [
-      {
-        product_ID: getRandomInt(1000, 1000000).toString(),
-        quantity: getRandomInt(1, 10),
-        title: randomWords(),
-        price: getRandomInt(10, 20),
-      },
-    ],
-  });
+  const order = getRandomInt(10, 20).toString();
+  res.json(createOrder(order));
 });
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function createOrder(orderNo) {
+  return {
+    OrderNo: orderNo,
+    createdBy: "Internal Inc.",
+    buyer: "The Buyer Co.",
+    currency: { code: "EUR" },
+    Items: [
+      {
+        product_ID: getRandomInt(1000, 1000000).toString(),
+        quantity: getRandomInt(1, 10),
+        title: randomWords(),
+        price: getRandomInt(10, 20),
+      },
+    ],
+  };
 }
