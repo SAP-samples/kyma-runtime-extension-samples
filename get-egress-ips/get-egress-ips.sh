7967cc98f38bd7e2c4ed281b854de8cd34a91d49
Gaurav Abbi <gaurav.abbi@sap.com>
2023-07-28 12:15:40 +0200

    Working with bash

--- get-egress-ips/get-egress-ips.sh
+++ get-egress-ips/get-egress-ips.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-overrides="{ \"apiVersion\": \"v1\", \"spec\": { \"nodeSelector\": { \"topology.kubernetes.io/zone\": \"$zone\" } } }"
 
-for zone in $(kubectl get nodes -o 'custom- columns=NAME:.metadata.name,REGION:.metadata.labels.topology\.kubernetes\.io/region,ZONE:.metadata.labels.topology\.kubernetes\.io/zone' -o json | jq -r '.items[].metadata.labels["topology.kubernetes.io/zone"]' | sort | uniq);
-do
-    kubectl run -i --tty busybox --image=yauritux/busybox-curl --restart=Never --overrides="$overrides" --rm --command -- curl http://ifconfig.me/ip >>/tmp/cluster_ips 2>/dev/null
+ZONES=$(kubectl get nodes -o jsonpath='{.items[*].metadata.labels.topology\.kubernetes\.io/zone}')
+
+ZONES=$(for i in ${ZONES[@]}; do echo $i; done | sort -u)
+
+for zone in $ZONES; do overrides="{ \"apiVersion\": \"v1\", \"metadata\" : { \"labels\": { \"sidecar.istio.io/inject\" : \"false\" } }, \"spec\": { \"nodeSelector\": { \"topology.kubernetes.io/zone\": \"$zone\" } } }"
+kubectl run -i --tty busybox --image=yauritux/busybox-curl --restart=Never --overrides="$overrides" --rm --command -- curl http://ifconfig.me/ip >>/tmp/cluster_ips 2>/dev/null
 done
-awk '{gsub("pod \"busybox\" deleted", "", $0); print}' /tmp/cluster_ips rm /tmp/cluster_ips
+
+awk '{gsub("pod \"busybox\" deleted", "", $0); print}' /tmp/cluster_ips 
+rm /tmp/cluster_ips
+
