dbe2a44362c39f5b37581ad07f15876b9f5989c5
Gaurav Abbi <gaurav.abbi@sap.com>
2023-07-28 13:27:31 +0200

    use the docker sponsored oss image with proper signature

--- get-egress-ips/get-egress-ips.sh
+++ get-egress-ips/get-egress-ips.sh
@@ -5,7 +5,7 @@ ZONES=$(kubectl get nodes -o jsonpath='{.items[*].metadata.labels.topology\.kube
 ZONES=$(for i in ${ZONES[@]}; do echo $i; done | sort -u)
 
 for zone in $ZONES; do overrides="{ \"apiVersion\": \"v1\", \"metadata\" : { \"labels\": { \"sidecar.istio.io/inject\" : \"false\" } }, \"spec\": { \"nodeSelector\": { \"topology.kubernetes.io/zone\": \"$zone\" } } }"
-kubectl run -i --tty busybox --image=yauritux/busybox-curl --restart=Never --overrides="$overrides" --rm --command -- curl http://ifconfig.me/ip >>/tmp/cluster_ips 2>/dev/null
+kubectl run -i --tty busybox --image=curlimages/curl --restart=Never --overrides="$overrides" --rm --command -- curl http://ifconfig.me/ip >>/tmp/cluster_ips 2>/dev/null
 done
 
 awk '{gsub("pod \"busybox\" deleted", "", $0); print}' /tmp/cluster_ips 
